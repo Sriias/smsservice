474001e799f3fbcaf90693e864e8ce815fd5441a
unknown <l.sridhar@SWAVD.Desktopready.com>
2024-07-23 07:28:43 +0000

    sourcesms

---
+++ Controllers/SmsReceiverController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using Azure.Communication.Sms;
+using Azure.Messaging.EventGrid;
+using Azure.Messaging.EventGrid.SystemEvents;
+
+[ApiController]
+[Route("[controller]")]
+public class SmsReceiverController : ControllerBase
+{
+    private readonly SmsClient _smsClient;
+    private readonly EventGridPublisherClient _eventGridClient;
+
+    public SmsReceiverController()
+    {
+        string connectionString = "<YOUR_CONNECTION_STRING>";
+        _smsClient = new SmsClient(connectionString);
+
+        // Replace with your Event Grid Topic endpoint and key
+        _eventGridClient = new EventGridPublisherClient(
+            new Uri("<YOUR_EVENT_GRID_TOPIC_ENDPOINT>"),
+            new AzureKeyCredential("<YOUR_EVENT_GRID_TOPIC_KEY>")
+        );
+    }
+
+    [HttpPost("receive")]
+    public IActionResult ReceiveSms([FromBody] EventGridEvent eventGridEvent)
+    {
+        if (eventGridEvent.EventType == EventGridEventTypes.SmsReceivedEventData)
+        {
+            var smsReceivedEventData = eventGridEvent.Data.ToObjectFromJson<SmsReceivedEventData>();
+            Console.WriteLine($"Received SMS from: {smsReceivedEventData.From}");
+            Console.WriteLine($"Message: {smsReceivedEventData.Message}");
+        }
+
+        return Ok();
+    }
+}
